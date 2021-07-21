32c96d791017d08b94aefe62492db70745603692
Isabella dos Santos Sobral <82097115+isabellassobral@users.noreply.github.com>
2021-07-21 17:17:13 -0300

    Delete simulado directory
    Não finalizados

--- simulado/03-simulado.rb
+++
@@ -1,7 +0,0 @@
-# 3) Escreva uma função chamada "calcula_media" que deve receber um array de números como parâmetro e deve retornar o valor da média aritmética entre todos os números do array.
-#     puts(calcula_media([1, 2, -1, 3, 0, 7]))  # deve imprimir 2.0
-
-def calcula_media(array)
-    soma = []
-    for n in (0..array.size-1)
-        
